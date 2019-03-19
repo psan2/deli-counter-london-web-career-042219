d799136357593df673858345eaa77a05c912abd7
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-19 18:16:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,6 @@ def take_a_number(line, name)
   line.push(name)
   puts "Welcome, #{name}. You are number #{line.length} in line."
 end
+
+def now_serving(line)
+  puts "Currently serving #{line.unshift}."
