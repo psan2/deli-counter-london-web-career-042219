f02f14d084b5990a4acd08b0ab899f1358f92d68
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-19 18:17:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 
 def now_serving(line)
   if line.length > 0
-    puts "Currently serving #{line.unshift}."
+    puts "Currently serving #{line.shift}."
   else
     puts "There is nobody waiting to be served!"
   end
