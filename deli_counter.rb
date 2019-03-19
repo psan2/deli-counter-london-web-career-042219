7fe71eb9311bfd62fb15aab8471a116ccb076a13
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-19 18:13:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(line)
+  if line.length == 0
+    puts "The line is currently empty."
+  else
+    temp = []
+    line.each_with_index do |name, index|
+      temp.push(" #{index+1}. #{name}")
+    end
+    puts "The line is currently:#{temp.join("")}"
+  end
+end
+
+def take_a_number(line, name)
+
+end
